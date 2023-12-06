01fbbdb6b611f5e4dbccb0b8b84ad2525517525e
1F10220082 <s1f102200828@iniad.org>
2023-12-06 18:51:29 +0900

    標準ログイン、ログアウトの基盤を作成

--- login/views.py
+++ login/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 
 def createUser(request):
     contexts = {}
+    if request.method == "POST":
+        password = request.POST.get("password")
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        Newuser = User.objects.create_user(name, email, password)
+        Newuser.save()
+        return render 
     return render(request, "registration/createUser.html", contexts)
