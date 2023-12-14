8d173ae10f0f6208a9ac7a4b68d70a292f0d5e9c
1F10220148 <s1f102201481@iniad.org>
2023-12-14 17:15:55 +0900

    formを修正

--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from .forms import FormDefUser
 
 
 def createDefUser(request):
