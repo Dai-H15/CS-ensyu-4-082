561a9acd8aba00057aaaf2f179b26fb8de8a4eb8
1F10220082 <s1f102200828@iniad.org>
2023-12-15 21:06:46 +0900

    所属コミュニティーの限定と修正

--- customUser/views.py
+++ customUser/views.py
@@ -26,6 +26,15 @@ def Check_is_RegistByEmail(self):
         return True
 
 
+def Check_Community(PersonalData, community):
+    user_mail_addr = PersonalData.email
+    community_name = community.community_name
+    if re.search(community_name, user_mail_addr) is None:
+        return False
+    else:
+        return True
+
+
 def CreateCustomUser(request):
     contexts = {}
     if request.method == "POST":
@@ -37,12 +46,21 @@ def CreateCustomUser(request):
             data.custom_user_Name = form.cleaned_data.get("custom_user_Name")
             data.custom_user_key = secrets.token_hex(32)
             data.Community = CollectCommunity(request)
-            data.save()
-            contexts["message"] = "登録が完了しました"
-            contexts["res"] = "0"
+            if data.Community.is_RegistByEmail:
+                if Check_Community(CollectPersonalData(request), CollectCommunity(request)):
+                    data.save()
+                    contexts["message"] = "登録が完了しました。限定コミュニティーに所属しています"
+                    contexts["res"] = "0"
+                else:
+                    contexts["message"] = "このコミュニティーには参加できません。"
+                    contexts["res"] = "1"
+            else:
+                data.save()
+                contexts["message"] = "登録が完了しました。公開コミュニティーに所属しています"
+                contexts["res"] = "0"
         else:
             contexts["message"] = "登録に失敗しました。入力値を確かめてください"
-            contexts["res"] = "0"
+            contexts["res"] = "1"
     users = ShowCustomUsers(request)
     contexts["CustomUsers"] = users
     contexts["form"] = CustomUserForm
