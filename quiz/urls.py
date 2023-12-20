b9d7193094d844c326d57bb6ffa8f05e1805d67a
1F10220148 <s1f102201481@iniad.org>
2023-12-20 23:57:29 +0900

    quizページの制作開始

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path, include
+from . import views as quizViews
 
-urlpatterns = []
+urlpatterns = [
+    path("quiz/", quizViews.index, name="quiztop"),
+    path("quiz/make", quizViews.make, name="make"),
+    path("quiz/play", quizViews.play, name="play")
+]
--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    contexts = {}
+    return render(request, "quiz/index.html", contexts)
+
+def make(request):
+    contexts = {}
+    return render(request, "quiz/make.html", contexts)
+
+def play(request):
+    contexts = {}
+    return render(request, "quiz/play.html", contexts)
