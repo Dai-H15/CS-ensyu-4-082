4b6b26429032c0ea006fbf9c7f1ebb925dab5a0e
1F10220148 <s1f102201481@iniad.org>
2024-01-10 20:09:35 +0900

    customuserIconからカスタムユーザの変更とログアウトを実装

--- portal/urls.py
+++ portal/urls.py
@@ -3,6 +3,6 @@ from . import views as portalViews
 urlpatterns = [
     path("", portalViews.firstShow),
     path("portal/", portalViews.index, name="portal"),
-    path("redirect_app/<str:to>", portalViews.redirect_app, name="redirect_app"),
+    path("redirect_app/<str:to>/", portalViews.redirect_app, name="redirect_app"),
 
 ]
